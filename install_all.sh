eb48491b06ad8acf8e2094e3be1b3ed2b784dc17
lukewebdev <lukewebdev@gmail.com>
2020-02-09 00:42:22 -0600

    Update install_all.sh
    Switch to release/0.7-melodic or release/0.8-melodic for kobuki git repos

--- install_all.sh
+++ install_all.sh
@@ -9,6 +9,27 @@ git clone https://github.com/turtlebot/turtlebot_apps.git
 git clone https://github.com/turtlebot/turtlebot_msgs.git
 git clone https://github.com/turtlebot/turtlebot_interactions.git
 
+git clone https://github.com/yujinrobot/kobuki_msgs.git
+cd kobuki_msgs
+git checkout release/0.7-melodic
+cd ../
+
+git clone https://github.com/yujinrobot/kobuki_desktop.git
+cd kobuki_desktop
+git checkout release/0.7-melodic
+cd ../
+
+git clone https://github.com/yujinrobot/kobuki.git
+cd kobuki
+release/0.7-melodic
+cd ../
+
+git clone https://github.com/yujinrobot/yujin_ocs.git
+cd yujin_ocs
+git checkout release/0.8-melodic
+cd ../
+
+
 git clone https://github.com/toeklk/orocos-bayesian-filtering.git
 cd orocos-bayesian-filtering/orocos_bfl/
 ./configure
@@ -21,19 +42,6 @@ cd ../
 git clone https://github.com/udacity/robot_pose_ekf
 git clone https://github.com/ros-perception/depthimage_to_laserscan.git
 
-git clone https://github.com/yujinrobot/kobuki_msgs.git
-git clone https://github.com/yujinrobot/kobuki_desktop.git
-cd kobuki_desktop/
-rm -r kobuki_qtestsuite
-cd -
-git clone https://github.com/yujinrobot/kobuki.git
-mv kobuki/kobuki_description kobuki/kobuki_bumper2pc \
-  kobuki/kobuki_node kobuki/kobuki_keyop \
-  kobuki/kobuki_safety_controller ./
-
-git clone https://github.com/yujinrobot/yujin_ocs.git
-mv yujin_ocs/yocs_cmd_vel_mux yujin_ocs/yocs_controllers .
-rm -rf yujin_ocs
 
 sudo apt-get install ros-melodic-kobuki-* -y
 sudo apt-get install ros-melodic-ecl-streams -y
